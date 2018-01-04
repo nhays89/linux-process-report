22da94dd632e6eb71af7bed4f557af6c5675458e
nicholas hays <nicholas.hays@hotmail.com>
2018-01-04 13:59:37 -0800

    sanitized code

--- procReport.c
+++ procReport.c
@@ -1,3 +1,6 @@
+//author: Nicholas Hays
+//project: Report on all linux proccess state.
+
 #include<linux/module.h>
 #include<linux/proc_fs.h>
 #include<linux/seq_file.h>
@@ -25,17 +28,17 @@ struct proc_glo_data {
 	int stopped;
 };
 
- void print_proc_report(struct seq_file *m, struct proc_data **pd, struct proc_glo_data *pgd) {
+void print_proc_report(struct seq_file *m, struct proc_data **pd, struct proc_glo_data *pgd) {
 	int i;
 	int procLen = pgd->runnable + pgd->unrunnable + pgd->stopped;
-seq_printf(m, "PROCESS REPORTER:\nUNRUNNABLE: %d\nRUNNABLE: %d\nSTOPPED: %d\n", pgd->unrunnable, pgd->runnable, pgd->stopped);
+	seq_printf(m, "PROCESS REPORTER:\nUNRUNNABLE: %d\nRUNNABLE: %d\nSTOPPED: %d\n", pgd->unrunnable, pgd->runnable, pgd->stopped);
 	for(i = 0; i < procLen; i++) {
-if(pd[i]->numOfChildren == 0) {
-		seq_printf(m, "Process ID=%d Name=%s *No Children \n",pd[i]->parent_id, pd[i]->parent_name);
-	} else {
-		seq_printf(m, "Process ID=%d Name=%s number_of_children=%d first_child_pid=%d first_child_name=%s \n",pd[i]->parent_id, pd[i]->parent_name, pd[i]->numOfChildren, pd[i]->first_child_pid, pd[i]->first_child_name);	
-	}
-	
+		if(pd[i]->numOfChildren == 0) {
+			seq_printf(m, "Process ID=%d Name=%s *No Children \n",pd[i]->parent_id, pd[i]->parent_name);
+		} else {
+			seq_printf(m, "Process ID=%d Name=%s number_of_children=%d first_child_pid=%d first_child_name=%s \n",pd[i]->parent_id, pd[i]->parent_name, pd[i]->numOfChildren, pd[i]->first_child_pid, pd[i]->first_child_name);	
+		}
+		
 	}
 	
 }
@@ -43,18 +46,18 @@ if(pd[i]->numOfChildren == 0) {
 
 
 static int hello_proc_show(struct seq_file *m, void *v) {
-int buffCount = 1;
-int numOfProcs = 0;
-long state =0;
-struct task_struct *parent_task;
-struct proc_data **pd = NULL;
-struct proc_glo_data *pgd = kmalloc(sizeof(struct proc_glo_data), GFP_KERNEL);
+	int buffCount = 1;
+	int numOfProcs = 0;
+	long state =0;
+	struct task_struct *parent_task;
+	struct proc_data **pd = NULL;
+	struct proc_glo_data *pgd = kmalloc(sizeof(struct proc_glo_data), GFP_KERNEL);
 
-pgd->runnable = 0;
-pgd->unrunnable = 0;
-pgd->stopped = 0;
+	pgd->runnable = 0;
+	pgd->unrunnable = 0;
+	pgd->stopped = 0;
 	for_each_process(parent_task){
-	
+		
 		if(numOfProcs % BUFFER_SIZE == 0) {
 			buffCount++;
 			pd = krealloc(pd,  BUFFER_SIZE * buffCount * sizeof(struct proc_data *), GFP_KERNEL);
@@ -62,73 +65,71 @@ pgd->stopped = 0;
 				return 0;
 			}
 		}
-	
+		
 		pd[numOfProcs] = kmalloc(sizeof(struct proc_data), GFP_KERNEL);
 		pd[numOfProcs]->parent_id = parent_task->pid;
 		pd[numOfProcs]->parent_name = parent_task->comm;
 		pd[numOfProcs]->numOfChildren = 0;
-	
-	
+		
+		
 		if(&parent_task->children != NULL) {
 			struct list_head *list; 
-			list_for_each(list, &parent_task->children) {
-	
-			struct task_struct *child_task_struct = list_entry(list, struct task_struct, sibling);
-			//seq_printf(m, "child name is: %s\n", child_task_struct->comm);
+			list_for_each(list, &parent_task->children) {	
+				struct task_struct *child_task_struct = list_entry(list, struct task_struct, sibling);
 				if(pd[numOfProcs]->numOfChildren == 0) {
 					pd[numOfProcs]->first_child_name = child_task_struct->comm;
 					pd[numOfProcs]->first_child_pid = child_task_struct->pid;
 				}
-			(pd[numOfProcs]->numOfChildren)++;
+				(pd[numOfProcs]->numOfChildren)++;
 			}
 		}
+		
+		state = parent_task->state;
+		if(state < 0) {
+			(pgd->runnable)++;
+		} else if(state == 0) {
+			(pgd->unrunnable)++;
+		} else {
+			(pgd->stopped)++;	
+		}
+
+		numOfProcs++;
+
+	}
+	print_proc_report(m, pd, pgd);
 	
-	state = parent_task->state;
-	if(state < 0) {
-	(pgd->runnable)++;
-	} else if(state == 0) {
-	(pgd->unrunnable)++;
-	} else {
-	(pgd->stopped)++;	
+	
+	
+	if(pd) {
+		kfree(pd);
+	}
+	if(pgd) {
+		kfree(pgd);
 	}
 
-numOfProcs++;
-
-}
-print_proc_report(m, pd, pgd);
-	
-	
-    	
-	if(pd) {
-   kfree(pd);
-}
-if(pgd) {
-	kfree(pgd);
-}
- 
-  return 0;
+	return 0;
 }
 
 
 static int hello_proc_open(struct inode *inode, struct  file *file) {
-  return single_open(file, hello_proc_show, NULL);
+	return single_open(file, hello_proc_show, NULL);
 }
 
 static const struct file_operations hello_proc_fops = {
-  .owner = THIS_MODULE,
-  .open = hello_proc_open,
-  .read = seq_read,
-  .llseek = seq_lseek,
-  .release = single_release,
+	.owner = THIS_MODULE,
+	.open = hello_proc_open,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.release = single_release,
 };
 
 
 
 int proc_init (void) {
 
- 	proc_create("proc_report", 0, NULL, &hello_proc_fops);
+	proc_create("proc_report", 0, NULL, &hello_proc_fops);
 	
-  return 0;
+	return 0;
 }
 
 void proc_cleanup(void) {
